0143ef49bc91b8a716a618a6a1f80550dfadd5b0
robitops10 <robitops10@gmail.com>
2021-01-12 14:01:51 +0600

    Better Error handling & Note

--- webpack-config.js
+++ webpack-config.js
@@ -2,11 +2,29 @@ const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 
+/*
++-------------------------------------[ System Limit Error ]----------------------------------------+
+|																																																		|
+| If build Process cross system default file read limit: to increase read file size: 								|
+|																																																		|
+| 		$ echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p 		|
+| 		$ npm start 																																									|
+|																																																		|
+| - More Linux.txt 	[ Max File Read Error ] 																												|
+|																																																		|
++---------------------------------------------------------------------------------------------------+
+
+- One More thing: Javascript can't directly read System level file so 	
+		 import json from './file.json' throw error 					// But React have, may be use NodeJS or fetch API
+
+*/
 
 const destination = path.resolve( __dirname, 'dist');
 
 module.exports = {
-	mode: 'development',
+	mode: 'development', 																		// Show Error in Console Too, Easy to Detact than Browser
+	devtool: 'eval-cheap-module-source-map', 								// Show Original Error File#Number insted of build File#number
+
 	devServer: {
 		contentBase: destination,
 		port: 9999,
